786b83eda986a930be6c4110767b995e8633f7d0
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-11 01:30:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+function updateDriverWithKeyAndValue() {
+  
+}
+
+function destructivelyUpdateDriverWithKeyAndValue() {
+  
+}
+
+function deleteFromDriverByKey() {
+  
+}
+
+function destructivelyDeleteFromDriverByKey() {
+  
+}
