80107543334e528609cbe4117c86ccf1d9639c74
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-11 01:40:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var driver = {};
 function updateDriverWithKeyAndValue(driver, key, value) {
   const newObject = {...driver};
   
+  newObject[key] = value;
   
+  return newObject;
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
